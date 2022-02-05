9cb616425cacb170ff4b8f9c20d85f89c4ed132e
nvitya <nvitya@gmail.com>
2022-02-05 10:39:51 +0100

    Original Espressif definitions included

--- rv32i/ESP/32C3/esp32c3_vihal.h
+++ rv32i/ESP/32C3/esp32c3_vihal.h
@@ -20,9 +20,12 @@
  * --------------------------------------------------------------------------- */
 /*
  *  file:     esp32c3_vihal.h
- *  brief:    VIHAL ESP32C3 include and own definitions
- *            the original Espressif peripheral definitions are not so usable
- *            so I made my own peripheral structs like the most ARM vendors provide
+ *  brief:    VIHAL ESP32C3 register definitions
+ *            Inlcuding some original Espressif definitions from ESP-IDF: https://github.com/espressif/esp-idf
+ *            The soc defition files are here:
+ *               esp-idf/components/soc/esp32c3/include/soc/
+ *
+ *            I made my own peripheral structs for some peripherals like GPIO
  *  version:  1.00
  *  date:     2022-02-03
  *  authors:  nvitya
@@ -157,12 +160,94 @@ typedef struct
 //
 } esp_io_mux_t;
 
+
+typedef struct
+{
+  volatile uint32_t      CPU_PERI_CLK_EN;    // 0000
+  volatile uint32_t      CPU_PERI_RST_EN;    // 0004
+  volatile uint32_t      CPU_PER_CONF;       // 0008
+  uint32_t               _pad_000C;
+  volatile uint32_t      PERIP_CLK_EN0;      // 0010
+  volatile uint32_t      PERIP_CLK_EN1;      // 0014
+  volatile uint32_t      PERIP_RST_EN0;      // 0018
+  volatile uint32_t      PERIP_RST_EN1;      // 001C
+  uint32_t               _pad_0020;
+  volatile uint32_t      BT_LPCK_DIV_FRAC;   // 0024
+  volatile uint32_t      CPU_INTR_FROM_CPU_0;// 0028
+  volatile uint32_t      CPU_INTR_FROM_CPU_1;// 002C
+  volatile uint32_t      CPU_INTR_FROM_CPU_2;// 0030
+  volatile uint32_t      CPU_INTR_FROM_CPU_3;// 0034
+  volatile uint32_t      RSA_PD_CTRL;        // 0038
+  uint32_t               _pad_003C;
+  volatile uint32_t      CACHE_CONTROL;      // 0040
+  volatile uint32_t      EXTERNAL_DEVICE_ENCRYPT_DECRYPT_CONTROLLER;      // 0044
+  volatile uint32_t      RTC_FASTMEM_CONFIG; // 0048
+  volatile uint32_t      RTC_FASTMEM_CRC;    // 004C
+  uint32_t               _pad_0050;
+  volatile uint32_t      CLOCK_GATE;         // 0054
+//
+} esp_sysreg_t;
+
+#if 1
+  // definition check:
+
+  static_assert(offsetof(esp_sysreg_t, BT_LPCK_DIV_FRAC)    == 0x024, "offset error");
+  static_assert(offsetof(esp_sysreg_t, CLOCK_GATE)          == 0x054, "offset error");
+#endif
+
+
+typedef struct
+{
+  uint32_t               _pad_0000[41];
+  volatile uint32_t      CLKGATE_FORCE_ON;   // 00A4
+  volatile uint32_t      MEM_POWER_DOWN;     // 00A8
+  volatile uint32_t      MEM_POWER_UP;       // 00AC
+//
+} esp_apb_ctrl_t;
+
 //-----------------------------------------------------------------------------
-// Instances
+// VIHAL definition Instances
 //-----------------------------------------------------------------------------
 
 #define GPIO         ((esp_gpio_t *)(0x60004000))
 #define IO_MUX     ((esp_io_mux_t *)(0x60009000))
 
-#define UART0              ((void *)(0x60000000))
-#define UART1              ((void *)(0x60010000))
+#define SYSTEM     ((esp_sysreg_t *)(0x600C0000))
+#define APB_CTRL ((esp_apb_ctrl_t *)(0x60026000))
+
+//-----------------------------------------------------------------------------
+// Espressif definitions
+//-----------------------------------------------------------------------------
+
+#include "soc.h"
+#include "soc_caps.h"
+
+#include "soc/apb_ctrl_reg.h"
+#include "soc/cache_memory.h"
+#include "soc/efuse_reg.h"
+#include "soc/extmem_reg.h"
+#include "soc/fe_reg.h"
+#include "soc/gdma_channel.h"
+#include "soc/gdma_reg.h"
+#include "soc/gpio_sig_map.h"
+#include "soc/hwcrypto_reg.h"
+#include "soc/i2c_reg.h"
+#include "soc/i2s_reg.h"
+#include "soc/interrupt_core0_reg.h"
+#include "soc/ledc_reg.h"
+#include "soc/nrx_reg.h"
+#include "soc/periph_defs.h"
+#include "soc/rmt_reg.h"
+#include "soc/rtc_cntl_reg.h"
+#include "soc/spi_mem_reg.h"
+#include "soc/spi_pins.h"
+#include "soc/spi_reg.h"
+#include "soc/syscon_reg.h"
+#include "soc/system_reg.h"
+#include "soc/systimer_reg.h"
+#include "soc/timer_group_reg.h"
+#include "soc/uart_channel.h"
+#include "soc/uart_reg.h"
+#include "soc/uhci_reg.h"
+
+
