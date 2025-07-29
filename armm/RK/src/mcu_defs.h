8bdcf91d77cb1c3a50f0650f6bf9032253253f8b
nvitya <nvitya@gmail.com>
2025-07-29 21:19:33 +0200

    some preliminary RK3506 definitions

---
+++ armm/RK/src/mcu_defs.h
@@ -0,0 +1,48 @@
+/* This file is a part of the VIHAL project: https://github.com/nvitya/vihal
+ * Copyright (c) 2021 Viktor Nagy, nvitya
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software. Permission is granted to anyone to use this
+ * software for any purpose, including commercial applications, and to alter
+ * it and redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software in
+ *    a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ * --------------------------------------------------------------------------- */
+/*
+ *  file:     mcu_defs.h (RK)
+ *  brief:    RK MCU Family definitions
+ *  date:     2025-07-29
+ *  authors:  nvitya
+*/
+
+#ifndef __MCU_DEFS_H
+#define __MCU_DEFS_H
+
+#if defined(MCUSF_35)
+
+  #if !defined(MAX_CLOCK_SPEED)
+    #define MAX_CLOCK_SPEED         200000000
+  #endif
+
+  #define MCU_INTERNAL_RC_SPEED    8000000
+
+#endif
+
+#if __CORTEX_M < 3
+  #define CLOCKCNT16       (0)
+#endif
+
+inline void __attribute__((always_inline)) mcu_preinit_code()
+{
+}
+
+#endif // __MCU_DEFS_H
